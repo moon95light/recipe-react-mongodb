c0b0d4504801a1c121e46c55a31eb1dc9843737d
antonius95 <140446918+antonius95@users.noreply.github.com>
2023-07-31 12:05:58 +0200

    add
    add

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+/* eslint-disable no-restricted-globals */
 
 export const Auth = () => {
     return (
@@ -13,22 +17,44 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    return (
-        <div className="auth-container">
-            <from>
-                <h2> Login </h2>
-                <div className="form-group">
-                    <label htmlFor="username">Username: </label>
-                    <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-                </div>
+    const [_, setCookies] = useCookies(["access_token"]);
 
-                <button type="submit">Login</button>
-            </from>
-        </div>
+    const navigate = useNavigate()
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:3001/auth/login", {
+                username,
+                password,
+            });
+            if (response.data.message == "User doesn't Exist!") {
+                navigate("/");
+                alert("User doesn't Exist!");
+            }
+            if (response.data.message == "Username or Password is incorrect") {
+                alert("Username or Password is incorrect.");
+                navigate("/");
+            }
+
+            setCookies("access_token", response.data.token);
+            window.localStorage.setItem("userID", response.data.userID);
+            navigate("/");
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <Form
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            label="Login"
+            onSubmit={onSubmit}
+        />
     );
 }
 
@@ -36,10 +62,39 @@ const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const onSubmit = async () => {
+        event.preventDefault();
+        try {
+            const aa = await axios.post("http://localhost:3001/auth/register", {
+                username,
+                password,
+            });
+            if (aa.data.message == "User already exists!")
+                alert("NO")
+            else
+                alert("registeration Competed! Now Login.")
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <Form
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            label="Register"
+            onSubmit={onSubmit}
+        />
+    );
+}
+
+const Form = ({ username, setUsername, password, setPassword, label, onSubmit, }) => {
     return (
         <div className="auth-container">
-            <from>
-                <h2> Register </h2>
+            <form onSubmit={onSubmit}>
+                <h2> {label} </h2>
                 <div className="form-group">
                     <label htmlFor="username">Username: </label>
                     <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
@@ -49,10 +104,8 @@ const Register = () => {
                     <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
                 </div>
 
-                <button type="submit">Register</button>
-            </from>
+                <button type="submit">{label}</button>
+            </form>
         </div>
-    );
+    )
 }
-
-const Form = ({username, setUsername, password, setPassword}) => {}
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -7,20 +7,36 @@ const router = express.Router()
 
 router.post("/register", async (req, res) => {
     const {username, password} = req.body;
-
     const user = await UserModel.findOne({ username });
 
     if(user) {
-        return res.json({ message: "User already exists!"});
-    }
+        return res.json({ message: "User already exists!"})
+    }else {
 
     const hashedPassword = await bcrypt.hash(password, 10)
     const newUser = new UserModel({username, password: hashedPassword});
     await newUser.save();
 
-    res.json({ message: "User Registered Successfully!" });
+    res.json({ message: "User Registered Successfully!" })
+    }
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
 
-export { router as userRouter}
+    if(!user) {
+        return res.json({message: "User doesn't Exist!"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+        return res.json({message: "Username or Password is incorrect"});
+    }
+
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({token, userID: user._id, })
+});
+
+export { router as userRouter }
