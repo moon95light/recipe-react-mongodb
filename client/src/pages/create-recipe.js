06da6b73e716a1dbb42d00c27418098152ddcdb9
antonius95 <140446918+antonius95@users.noreply.github.com>
2023-08-02 13:00:46 +0200

    update create-recipe
    create-recipe

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 export const CreateRecipe = () => {
     const [recipe, setRecipe] = useState({
         name: "",
@@ -9,21 +11,37 @@ export const CreateRecipe = () => {
     });
 
     const handleChange = (event) => {
+        const { name, value } = event.target;
+        setRecipe({ ...recipe, [name]: value })
     }
     return (
         <div className="create-recipe">
             <h2>Create Recipe</h2>
             <form>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" onChange={handleChange}/>
+                <input type="text"
+                    id="name"
+                    name="name"
+                    onChange={handleChange} />
+                    
                 <label htmlFor="ingredients">Ingredients</label>
 
                 <label htmlFor="instructions">Instructions</label>
-                <textarea id="instructions" name="instructions"></textarea>
+                <textarea
+                    id="instructions"
+                    name="instructions"
+                    onChange={handleChange}
+                ></textarea>
                 <label htmlFor="imageUrl">Image URL</label>
-                <input type="text" id="imageUrl" name="imageUrl"/>
+                <input type="text"
+                    id="imageUrl"
+                    name="imageUrl"
+                    onChange={handleChange} />
                 <label htmlFor="cookingTime">Cooking Time(min)</label>
-                <input type="number" id="cookingTime" name="cookingTime"/>
+                <input type="number"
+                    id="cookingTime"
+                    name="cookingTime"
+                    onChange={handleChange} />
             </form>
         </div>
     );
